bcb350009f0bf1e07101b5721038297ab24f6ab0
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-04-22 12:23:57 +0530

    Added search functionality on category

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -3,41 +3,54 @@ import Footer from "./footer";
 import Navbar from "./navbar";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
-import HomeDetail from "./home-detail.jpg";
 
 function Restaurants(){
 
     const [cat, setCat] = useState([]);
+    const [ searchTextFound, setSearchTextFound ] = useState(true);
 
     async function fetchCategories(){
         const res = await axios("https://www.themealdb.com/api/json/v1/1/categories.php");
-        console.log(res.data.categories);
+        // console.log(res.data.categories);
         setCat(res.data.categories);
     }
     // console.log(cat);    
 
     useEffect(()=>{
         fetchCategories();
-    });
+    },[]);
+
+    async function filterData(){
+        const input = document.getElementById("filter").value;
+        // console.log(input);
+        const res = await axios("https://www.themealdb.com/api/json/v1/1/categories.php");
+        // console.log(res.data.categories[2].strCategory);
+        const filtered = res.data.categories.filter(item => item.strCategory.toLowerCase().includes(input.toLowerCase()));
+        setCat(filtered);
+        setSearchTextFound(filtered.length > 0);
+    }
 
     return (
         <div>
             <Navbar/>
+            <input type="text" id="filter" onChange={filterData} placeholder="Search Category..."/>
             <div className="restaurants-wrapper">
             {
-                cat.map((item,id)=>{
-                    return(
-                        <div id="rest-cont" key={id}>
-                            <img src={item.strCategoryThumb} id="rest-img"/>
-                            <div id="rest-subcont">
-                                <h3>{item.strCategory}</h3>
-                                <NavLink to={`/category/${item.strCategory}`} className="visit-menu">
-                                    <lable id="rest-label">Visit Menu</lable>
-                                </NavLink>
+                searchTextFound ? (
+                    cat.map((item,id)=>{
+                        return(
+                            <div id="rest-cont" key={id}>
+                                <img src={item.strCategoryThumb} id="rest-img"/>
+                                <div id="rest-subcont">
+                                    <h3>{item.strCategory}</h3>
+                                    <NavLink to={`/category/${item.strCategory}`} className="visit-menu">
+                                        <label id="rest-label">Visit Menu</label>
+                                    </NavLink>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })
+                        )
+                    })
+                ) : <div id="notFound">No results found !</div>
             }
                         {/* return(<div key={id}>
                         <NavLink to={`/category/${item.strCategory}`}>
